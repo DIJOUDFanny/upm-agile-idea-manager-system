aad3b807954cd4170dd4dd67bf3a3780aa650fb0
Loïc Dubois-Termoz <loic.duboistermoz@gmail.com>
2021-11-19 01:02:44 +0100

    add editorconfig

--- src/main/java/es/upm/agile/entrepreneursideamanager/EntrepreneursIdeaManagerApplication.java
+++ src/main/java/es/upm/agile/entrepreneursideamanager/EntrepreneursIdeaManagerApplication.java
@@ -1,10 +1,7 @@
 package es.upm.agile.entrepreneursideamanager;
 
-import es.upm.agile.entrepreneursideamanager.model.entities.Idea;
-import es.upm.agile.entrepreneursideamanager.model.entities.IdeaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EntrepreneursIdeaManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EntrepreneursIdeaManagerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EntrepreneursIdeaManagerApplication.class, args);
+  }
 
-	@GetMapping("/hello")
-	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
+  @GetMapping("/hello")
+  public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 }
