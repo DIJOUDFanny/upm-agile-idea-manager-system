b5f97fb866788dbc855873cc11ad06787d68daf7
DIJOUDFanny <fanny.dijoud@etu.unice.fr>
2021-11-26 02:38:47 +0100

    icon like dislike

--- src/main/js/components/Idea.js
+++ src/main/js/components/Idea.js
@@ -17,8 +17,10 @@ class Idea extends React.Component {
         <th scope="row">{this.props.idea.id}</th>
         <td>{this.props.idea.name}</td>
         <td>{this.props.idea.description}</td>
-        <td><button type="button" class="btn btn-danger" onClick={this.handleDelete}>Delete</button></td>
-      </tr>
+        <i class="bi bi-hand-thumbs-up-fill">13</i>
+        <i class="bi bi-hand-thumbs-down-fill">2</i>
+        6
+        </tr>
     )
   }
 }
